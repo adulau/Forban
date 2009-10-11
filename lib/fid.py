a9359620480f9bd080d16b02a35fef21214f3387
Alexandre Dulaunoy <a@foo.be>
2009-10-11 16:36:41 +0200

    Forban UUID init, creation and regen added

--- lib/fid.py
+++ lib/fid.py
@@ -10,20 +10,34 @@ class manage:
 
         self.dynpath = dynpath
         self.fidpath = dynpath + "fid" 
-        
+        self.create()
+    
+    def create(self):
+ 
         if os.path.isdir(self.dynpath) and os.path.isfile(self.fidpath):
-            print "exit"
+            fidfile = open(self.fidpath,'r')
+            self.fid = fidfile.read()
+            fidfile.close()
         else:
-            self.fid = uuid.uuid4()
-            print "not existing"
+            self.fid = str(uuid.uuid4())
+            if not os.path.isdir(self.dynpath):
+                os.mkdir(self.dynpath)
+            fidfile = open(self.fidpath,'w')
+            fidfile.write(str(self.fid))
+            fidfile.close()
 
     def get (self):
         return self.fid
 
+    def regen (self):
+        os.unlink(self.fidpath)
+        self.create()
 
 def managetest():
-        fid = manage();
-        print fid.get();
+        fid = manage()
+        print fid.get()
+        fid.regen()
+        print fid.get()
 
 if __name__ == "__main__":
     
