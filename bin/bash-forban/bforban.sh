536914075310859d33bf744439c9d699e3cb5b65
Alexandre Dulaunoy <a@foo.be>
2010-12-31 16:00:11 +0100

    Forban bash client: search function added
    Now you can use the forban bash client to
    search for all the indexes dicovered. The
    script requires only Bash, curl and tcpdump.
    
    The script is used to show how easy is
    to build a software using the Forban protocol.
    
    A sample output:
    
    sudo bash bforban.sh  search unix
    
    Found Forban elliptic (5f916370-083f-4603-98ce-8051b70e71cb) http://192.168.1.5:12555/
    -> Unix/Albing - Bash Cookbook [unix shell programming] (O'Reilly, 2007).pdf,1999734
    -> Unix/Learning The vi And Vim Editors 7th Ed.pdf,7261405
    -> Unix/awk.txt,29190
    -> Unix/ProBash.pdf,1505268
    -> Unix/Effective-AWK-Programming.pdf,3488653
    -> Unix/OReilly Version Control with GIT.pdf,1901067
    -> Unix/LinuxJournal/Linux Journal 183 - July 2009.pdf,20481947
    -> Unix/LinuxJournal/Linux Journal 185 - September 2009.pdf,17966468
    -> Unix/LinuxJournal/Linux Journal 182 - June 2009.pdf,22835700
    -> Unix/LinuxJournal/Linux Journal 181 - May 2009.pdf,22174869
    -> Unix/LinuxJournal/Linux Journal 174 - October 2008.pdf,14520275
    -> Python/Gift - Python for Unix and Linux System Administration (O'Reilly, 2008).pdf,3585227
    -> Python/Python for Unix and Linux System Administration; Jason Brittain, Ian F. Darwin (O'Reilly, 2008).pdf,3204317

--- bin/bash-forban/bforban.sh
+++ bin/bash-forban/bforban.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 #
 # standalone Forban "client" written in Bash
-# 
-# Software required: tcpdump
 #
+# This is just a proof-of-concept to show how
+# easy it's to get standard something using
+# the forban protocol
+#
+# Software required: tcpdump and curl
 
 function GetAnnounce {
 sudo tcpdump -c 1 -t -p -n -l -A -s0 -ien1 "udp and port 12555 and udp[8:2]==0x666f" 2>/dev/null
 }
 
 function GetName {
-# $1 is the announce message
-awk 'NR==2' | cut -d';' -f3
+# input of tcpdump and just the forban packet
+awk '/forban/' | awk '{split ($1, a, "forban");print a[2]}' | cut -d';' -f3
 }
 
 function GetUUID {
-# $1 is the announce message
-awk 'NR==2' | cut -d';' -f5
+# input of tcpdump and just the forban packet
+awk '/forban/'| awk '{split ($1, a, "forban");print a[2]}' | cut -d';' -f5
 }
 
 function GetSourceIP {
@@ -29,6 +32,29 @@ function GetURL {
 #$1 is the IP
 awk '{print "http://" $1 ":12555/"}'
 }
+
+function GetIndex {
+URLP=$1
+INDEXSUFFIX="s/?g=forban/index"
+U=${URLP}${INDEXSUFFIX}
+echo ${U}
+curl -f -s "${U}"
+}
+function ForbanSearch {
+        while [ 1 ];
+        do
+                keywords=$1
+                A=`GetAnnounce`
+                X=`echo "${A}" | GetSourceIP`
+                UUID=`echo "${A}" | GetUUID`
+                NAME=`echo "${A}" | GetName`
+                URL=`echo ${X} | GetURL`
+                echo "Found Forban" ${NAME} "("${UUID}")" ${URL}
+                GetIndex ${URL} | grep -i ${keywords} | awk '{print "-> " $0}'
+                echo
+        done
+}
+
 
 function ForbanDiscover {
         while [ 1 ];
@@ -45,6 +71,9 @@ function ForbanDiscover {
 case "$1" in
         monitor)    ForbanDiscover
                     ;;
-        *)          echo $0 "[monitor]"
+        search)     keywords=$2
+                    ForbanSearch ${keywords}
+                    ;;
+        *)          echo $0 "[monitor] [search {keywords}]"
 esac
 
